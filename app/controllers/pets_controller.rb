a1c4da5d2fe6fac49c7f72945a5e49d16fa8155f
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-27 23:08:14 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,7 +11,6 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-
     @pet = Pet.create(params[:pet])
     if !params[:owner][:name].empty?
       @pet.owner = Owner.create(name: params[:owner][:name])
